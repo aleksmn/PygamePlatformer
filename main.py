a0e9a6455341d058f965f48a90a0b955406e5661
aleksmn <al-mn@yandex.ru>
2024-01-04 19:57:14 +0300

    init

--- main.py
+++ main.py
@@ -22,15 +22,8 @@ class Player(pg.sprite.Sprite):
     def __init__(self, map_width, map_height):
         super(Player, self).__init__()
 
-        # self.image = pg.Surface((50, 50))
-        # self.image.fill("red")
-
-        # Добавляем анимацию персонажа
-        self.load_animations()
-        self.current_animation = self.idle_animation_right
-        self.image = self.current_animation[0]
-        self.current_image = 0
-
+        self.image = pg.Surface((50, 50))
+        self.image.fill("red")
 
         self.rect = self.image.get_rect()
         self.rect.center = (200, 100)  # Начальное положение персонажа
@@ -47,97 +40,42 @@ class Player(pg.sprite.Sprite):
         self.interval = 200
 
 
-    def load_animations(self):
-        tile_scale = 2
-        tile_size = 32
-
-        self.idle_animation_right = []
-
-        num_images = 4
-        spritesheet = pg.image.load("sprites/Sprite Pack 3/4 - Tommy/Idle_Poses (32 x 32).png")
-
-        for i in range(num_images):
-            x = i * tile_size  # Начальная координата X изображения в спрайтшите
-            y = 0  # Начальная координата Y изображения в спрайтшите
-            rect = pg.Rect(x, y, tile_size, tile_size)  # Прямоугольник, который определяет область изображения
-            image = spritesheet.subsurface(rect)  # Вырезаем изображение из спрайтшита
-            image = pg.transform.scale(image, (tile_size * tile_scale, tile_size * tile_scale))
-            self.idle_animation_right.append(image)  # Добавляем изображение в список
-
-        self.idle_animation_left = [pg.transform.flip(image, True, False) for image in self.idle_animation_right]
-
-        self.move_animation_right = []
-
-        num_images = 8
-        spritesheet = pg.image.load("sprites/Sprite Pack 3/4 - Tommy/Running (32 x 32).png")
-
-        for i in range(num_images):
-            x = i * tile_size  # Начальная координата X изображения в спрайтшите
-            y = 0  # Начальная координата Y изображения в спрайтшите
-            rect = pg.Rect(x, y, tile_size, tile_size)  # Прямоугольник, который определяет область изображения
-            image = spritesheet.subsurface(rect)  # Вырезаем изображение из спрайтшита
-            image = pg.transform.scale(image, (tile_size * tile_scale, tile_size * tile_scale))
-            self.move_animation_right.append(image)  # Добавляем изображение в список
-
-        self.move_animation_left = [pg.transform.flip(image, True, False) for image in self.move_animation_right]
-
     def update(self, platforms):
-        # Обработка ввода
         keys = pg.key.get_pressed()
         if keys[pg.K_SPACE] and not self.is_jumping:
             self.jump()
 
         if keys[pg.K_a]:
-            if self.current_animation != self.move_animation_left:
-                self.current_animation = self.move_animation_left
-                self.current_image = 0
-
             self.velocity_x = -10
         elif keys[pg.K_d]:
-            if self.current_animation != self.move_animation_right:
-                self.current_animation = self.move_animation_right
-                self.current_image = 0
-
             self.velocity_x = 10
         else:
-            if self.current_animation not in (self.idle_animation_right, self.idle_animation_left):
-                if self.current_animation == self.move_animation_right:
-                    self.current_animation = self.idle_animation_right
-                elif self.current_animation == self.move_animation_left:
-                    self.current_animation = self.idle_animation_left
-                else:
-                    self.current_animation = self.idle_animation_left
-                self.current_image = 0
-
             self.velocity_x = 0
 
-        # Применение гравитации
-        self.velocity_y += self.gravity
-
-        # Предотвращение выхода за пределы карты по горизонтали
         new_x = self.rect.x + self.velocity_x
         if 0 <= new_x <= self.map_width - self.rect.width:
             self.rect.x = new_x
 
+        self.velocity_y += self.gravity
         self.rect.y += self.velocity_y
 
-        # Проверка на коллизии с платформами
         for platform in platforms:
-            if pg.sprite.collide_mask(self, platform):
-                if self.velocity_y > 0:
-                    self.rect.y = platform.rect.y - self.rect.height
-                    self.velocity_y = 0
-                elif self.velocity_y < 0:
-                    self.rect.y = platform.rect.y + platform.rect.height
-                    self.velocity_y = 0
+
+            if platform.rect.collidepoint(self.rect.midbottom):
+                self.rect.bottom = platform.rect.top
+                self.velocity_y = 0
                 self.is_jumping = False
 
-        if pg.time.get_ticks() - self.timer > self.interval:
-            self.current_image += 1
-            if self.current_image >= len(self.current_animation):
-                self.current_image = 0
-            self.image = self.current_animation[self.current_image]
-            self.timer = pg.time.get_ticks()
+            if platform.rect.collidepoint(self.rect.midtop):
+                self.rect.top = platform.rect.bottom
+                self.velocity_y = 0
+
+            if platform.rect.collidepoint(self.rect.midright):
+                self.rect.right = platform.rect.left
+
+            if platform.rect.collidepoint(self.rect.midleft):
+                self.rect.left = platform.rect.right
+
     def jump(self):
         self.velocity_y = -30
         self.is_jumping = True
